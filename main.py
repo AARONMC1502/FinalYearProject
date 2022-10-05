c9a6588cb8838dec6ff107b9ca33505c16839db6
Aaron <m017307k@student.staffs.ac.uk>
2022-10-05 11:57:34 +0100

    commit

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ import json
 f = open("pad.json")
 pad = json.load(f)
 
+# Shows how many characters are left in the pad
 print("left in pad: {0}".format(len(pad)))
+
+#plaintext message
 p_text = input("Message: ")
 
+#Generates and print cipher text
 c_text = encrypt(list(pad), p_text)
 print("Cipher text: {0}".format(c_text))
 
+#Decrypts cipher text and print original plain text
 d_text = decrypt(pad, c_text)
 print("Decrypted text: {0}".format(d_text))
