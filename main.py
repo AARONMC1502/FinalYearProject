80929f0d87d7daade88ff5e2ed1ed94d5d09cd0a
Aaron <m017307k@student.staffs.ac.uk>
2022-10-05 11:54:54 +0100

    commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ print("left in pad: {0}".format(len(pad)))
 p_text = input("Message: ")
 
 c_text = encrypt(list(pad), p_text)
+print("Cipher text: {0}".format(c_text))
 
-print(c_text)
-print(decrypt(pad, c_text))
+d_text = decrypt(pad, c_text)
+print("Decrypted text: {0}".format(d_text))
