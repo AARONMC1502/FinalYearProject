3dbb37bc9e79a9837109189548a448a9b85334fe
Aaron <m017307k@student.staffs.ac.uk>
2022-10-05 10:49:37 +0100

    removed space inclusion for decrypt

--- Decrypt.py
+++ Decrypt.py
@@ -5,9 +5,6 @@ def decrypt(pad, message):
     plainText = ""
     cipherText = [*message]
     for i in range(len(cipherText)):
-        if cipherText[i] == " ":
-            plainText += cipherText[i]
-            continue
         cipherIndex = (alphabet.index(cipherText[i]) - pad[i]) % 100
         plainText += alphabet[cipherIndex]
     return plainText
