cbc057a9e87848ccfb26be046d03094d79cec478
Aaron <m017307k@student.staffs.ac.uk>
2022-10-17 23:03:43 +0100

    Corrected issue with decoding only numbers, needed to add a 0 to the start of binary if MOD is more than 0

--- DecryptV2.py
+++ DecryptV2.py
@@ -2,6 +2,9 @@ def decryptV2(cipherBin, pad):
     a = 0
     plainBin = ""
 
+    if len(cipherBin) % 8 > 0:
+        plainBin += "0"
+
     for char in [*cipherBin]:
         a += 1
         plainBin += str(int(char) ^ int([*pad][a]))
