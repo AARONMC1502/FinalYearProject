945da5a2aaf713e75f858fd584b2416d75aac5ab
Aaron <m017307k@student.staffs.ac.uk>
2022-10-26 15:35:03 +0100

    Adjusted aspects of code to run on python 3.11

--- DecryptV2.py
+++ DecryptV2.py
@@ -8,8 +8,8 @@ def decrypt(cipherBin, pad):
         for i in range(8 - x):
             plainBin += "0"
 
-    for bit in [*cipherBin]:
+    for bit in list(cipherBin):
         a += 1
-        plainBin += str(int(bit) ^ int([*pad][a]))
+        plainBin += str(int(bit) ^ int(list(pad)[a]))
 
     return plainBin
--- EncryptV2.py
+++ EncryptV2.py
@@ -4,13 +4,16 @@ import UsedPad
 
 def encrypt(message, pad):
     cipherBin = ""
-    binMessage = Conversions.toBinary(message, "utf-8")
+
+    print(list(message))
+
+    binMessage = list(Conversions.toBinary(message, "utf-8"))
 
     a = 0
 
-    for bit in [*binMessage]:
+    for bit in binMessage:
         a += 1
-        cipherBin += str(int(bit) ^ int([*pad][a]))
+        cipherBin += str(int(bit) ^ int(list(pad)[a]))
 
     UsedPad.usedPad(len(binMessage))
     return cipherBin
