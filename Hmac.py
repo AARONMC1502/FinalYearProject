1a55075ec7efc3b5b3e3c995adbd44be28d75546
AARONMC1502 <102529375+AARONMC1502@users.noreply.github.com>
2022-11-17 10:32:40 +0000

    Update Hmac.py

--- Hmac.py
+++ Hmac.py
@@ -1,9 +1,9 @@
 import hmac
 import hashlib
 
-def calc_mac(used_key, message):
-    used_key = bytes(used_key, "utf-8")
+def calc_mac(key, message):
+    key = bytes(key, "utf-8")
     message = bytes(message, "utf-8")
 
-    messageHmac = hmac.new(used_key, message, hashlib.sha256)
+    messageHmac = hmac.new(key, message, hashlib.sha256)
     return messageHmac.hexdigest()
