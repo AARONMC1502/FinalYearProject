cc0a65daa761e4d0480c5927943b15dedcd59995
AARONMC1502 <102529375+AARONMC1502@users.noreply.github.com>
2022-11-17 10:46:37 +0000

    Update DecryptV2.py

--- DecryptV2.py
+++ DecryptV2.py
@@ -6,28 +6,28 @@ def decrypt(cipherBin, pad):
     a = 0
     counter = 0
 
-    plainBin = ""
+    FormattedPlainBin = ""
     pad = Conversions.toBinary(pad, "utf-8")
 
     x = len(cipherBin) % 8
 
     if len(cipherBin) % 8 > 0:
         for i in range(8 - x):
-            plainBin += "0"
+            FormattedPlainBin += "0"
             counter += 1
 
     for bit in list(cipherBin):
         a += 1
-        plainBin += str(int(bit) ^ int(list(pad)[a]))
+        formattedPlainBin += str(int(bit) ^ int(list(pad)[a]))
 
     #this formats the plainbin the same as it was on the encryption sideso
-    originalPlainBin = plainBin[counter:-32]
+    originalPlainBin = formattedPlainBin[counter:-32]
 
     messageHMAC = Conversions.toBinary(Hmac.calc_mac(pad, str(originalPlainBin)), "utf-8")
 
-    if messageHMAC[-32:] == plainBin[-32:]:
+    if messageHMAC[-32:] == formattedPlainBin[-32:]:
         print("valid authentication")
     else:
         print("invalid authentication")
 
-    return plainBin
+    return formattedPlainBin
